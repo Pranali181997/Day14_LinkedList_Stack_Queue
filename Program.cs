aca0497161cac54ab07717b74ef394f325956bcb
Pranali <pranalilambat00@gmail.com>
2022-02-25 11:17:51 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UC_1_LinkList List = new UC_1_LinkList();
+            //List.Add(30);
+            //List.Add(56);
+            //List.Add(70);
+            //List.Display();
+            //Console.WriteLine("....................................... ");
+
+            //List.Insert(1, 30);
+            //Console.WriteLine("After Inserting the elment in the linked list is ");
+            //List.Display();
+
+            //Console.WriteLine("............................................... ");
+            //List.Add(30);
+            //List.Add(56);
+            //List.Add(70);
+            //List.RemoveLastNode();
+            //Console.WriteLine("After removing last elment the linked list is " );
+            //List.Display();
+            //Console.WriteLine("...........................................................");
+
+            //List.Add(30);
+            //List.Add(56);
+            //List.Add(70);
+            //List.RemoveFirsttNode();
+            //Console.WriteLine("After removing First elment the linked list is ");
+            //List.Display();
+
+
+            //// LinkedList link4 = new LinkedList();
+            //List.Add(56);
+            //List.Add(30);
+            //List.Add(70);
+            //int search = List.Search(30);
+            //Console.WriteLine(search);
+
+
+            //LinkedList link5 = new LinkedList();
+            //List.Add(56);
+            //List.Add(30);
+            //List.Add(70);
+            //int position = List.Search(30);
+            //position++;
+            //List.InsertAtParticularPosition(position, 40);
+            //List.Display();
+
+
+            ////LinkedList link6 = new LinkedList();
+            //List.Add(56);
+            //List.Add(30);
+            //List.Add(70);
+            //List.Display();
+            //int position1 = List.Search(40);
+            //List.DeleteNodeAtParticularPosition(position1);
+
+
+
+
+
+
+
+        }
+    }
+}
